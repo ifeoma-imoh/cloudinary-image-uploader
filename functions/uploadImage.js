1cf08247768819ecb88081f9bf941daa42eec241
Ifeoma Imoh <60473225+ifeoma-imoh@users.noreply.github.com>
2021-10-16 12:09:12 +0400

    Update uploadImage.js

--- functions/uploadImage.js
+++ functions/uploadImage.js
@@ -17,6 +17,11 @@ exports.handler = async event => {
   });
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    };
     body: JSON.stringify(res)
   };
 };
