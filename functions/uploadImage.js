ed929efacdf68207d946f3179eec6ab45ddf700b
Ifeoma Imoh <60473225+ifeoma-imoh@users.noreply.github.com>
2021-10-16 12:22:40 +0400

    Update uploadImage.js

--- functions/uploadImage.js
+++ functions/uploadImage.js
@@ -5,24 +5,33 @@ dotenv.config();
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+exports.handler = async (event) => {
 
-exports.handler = async event => {
-  const file = event.body;
-  const res = await cloudinary.uploader.upload(file, {
-    image_metadata: true,
-  });
-  if (event.httpMethod !== 'POST') {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  };
+
+  if (event.httpMethod === "OPTIONS") {
     return {
       statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-      },
-      body: JSON.stringify(res)
+      headers,
+      body: JSON.stringify({ message: "Successful preflight call." }),
+    };
+  } else if (event.httpMethod === "POST") {
+    const file = event.body;
+    const res = await cloudinary.uploader.upload(file, {
+      image_metadata: true,
+    });
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(res),
     };
   }
 };
